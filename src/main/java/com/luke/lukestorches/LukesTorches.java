75d09666e6fa13278207d882b9b6ae2ff08c494d
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-05-29 22:45:09 +0200

    added first item - end torch

--- src/main/java/com/luke/lukestorches/LukesTorches.java
+++ src/main/java/com/luke/lukestorches/LukesTorches.java
@@ -1,10 +1,16 @@
 package com.luke.lukestorches;
 
+import com.luke.lukestorches.init.ItemInit;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -23,18 +29,29 @@ public class LukesTorches
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "lukestorches";
+    public static final CreativeModeTab TORCHES_TAB = new CreativeModeTab(MOD_ID) {
+        @Override
+        @OnlyIn(Dist.CLIENT)
+        public ItemStack makeIcon() {
+            return new ItemStack(ItemInit.END_TORCH.get());
+        }
+    };
 
     public LukesTorches()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+//        // Register the setup method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+//        // Register the enqueueIMC method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+//        // Register the processIMC method for modloading
+//        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
-        // Register ourselves for server and other game events we are interested in
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ItemInit.ITEMS.register(bus);
+
         MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     private void setup(final FMLCommonSetupEvent event)
---
+++ src/main/java/com/luke/lukestorches/init/ItemInit.java
@@ -0,0 +1,20 @@
+package com.luke.lukestorches.init;
+
+import com.google.common.base.Supplier;
+import com.luke.lukestorches.LukesTorches;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, LukesTorches.MOD_ID);
+
+    public static final RegistryObject<Item> END_TORCH = register("end_torch",
+            () -> new Item(new Item.Properties().tab(LukesTorches.TORCHES_TAB)));
+
+    private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item){
+        return ITEMS.register(name, item);
+    }
+}
